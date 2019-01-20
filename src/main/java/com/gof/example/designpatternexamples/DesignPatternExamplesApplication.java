8e0046497c0f3406141e5ba9bcc384bb600172fe
Rishi Kumar <rishikesh2111@gmail.com>
2019-01-20 12:37:55 +0530

    project skeleton commit

---
+++ src/main/java/com/gof/example/designpatternexamples/DesignPatternExamplesApplication.java
@@ -0,0 +1,14 @@
+package com.gof.example.designpatternexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternExamplesApplication.class, args);
+	}
+
+}
+
